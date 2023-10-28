087a13b7211951064f403728af72336239e23ad1
jkimdev <jkim.dev@icloud.com>
2023-10-28 10:21:06 +0900

    Initial Commit

---
+++ sockdak-server/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sockdak-server
+//
+//  Created by Jimmy on 2023/10/28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
