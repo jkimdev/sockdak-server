e9f8e9e048b98b4ac7b4fd9957749ba786cecc77
김재민 <jimmy@gimjaemins-MacBook-Air.local>
2023-11-08 23:03:39 +0900

    multi thread

--- sockdak-server/main.cpp
+++ sockdak-server/main.cpp
@@ -16,6 +16,45 @@
 #define SERVERPORT 9000
 #define BUFSIZE 512
 
+void *ProcessClient(void *arg) {
+    int client_sock = (int)(long long)arg;
+    int retval;
+    struct sockaddr_in clientaddr;
+    char addr[INET_ADDRSTRLEN];
+    socklen_t addrlen;
+    char buf[BUFSIZE + 1];
+    
+    // client info
+    addrlen = sizeof(clientaddr);
+    getpeername(client_sock, (struct sockaddr*)&clientaddr, &addrlen);
+    inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+    
+    // Communication with Client
+    while (1) {
+        // receive
+        retval = recv(client_sock, buf, BUFSIZE, MSG_WAITALL);
+        if (retval == -1) {
+            err_display("recv()");
+            break;
+        }
+        else if (retval == 0)
+            break;
+        // print received data
+        buf[retval] = '\0';
+        printf("[TCP/%s:%d] %s\n", addr, ntohs(clientaddr.sin_port), buf);
+        
+        // send data
+        retval = send(client_sock, buf, retval, 0);
+        if (retval == -1) {
+            err_display("send()");
+            break;
+        }
+    }
+    close(client_sock);
+    printf("[TCP Server] IP: %s:%d\n", addr, ntohl(clientaddr.sin_port));
+    return 0;
+}
+
 
 int main(int argc, const char * argv[]) {
     int retval;
@@ -40,7 +79,7 @@ int main(int argc, const char * argv[]) {
     int client_sock = 0;
     struct sockaddr_in clientAddr;
     socklen_t addrlen;
-    char buf[BUFSIZE + 1];
+    pthread_t tid;
     
     while (1) {
         // aceept
@@ -50,38 +89,14 @@ int main(int argc, const char * argv[]) {
             err_display("accept()");
             break;
         }
-        
-        // client info
         char addr[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &clientAddr.sin_addr, addr, sizeof(addr));
         printf("\n[TCP Server] Client IP: %s, Port: %hu\n", addr, ntohs(clientAddr.sin_port));
         
-        // Communication with Client
-        while (1) {
-            // receive
-            retval = recv(client_sock, buf, BUFSIZE, 0);
-            if (retval == -1) {
-                err_display("recv()");
-                break;
-            }
-            else if (retval == 0)
-                break;
-            // print received data
-            buf[retval] = '\0';
-            printf("[TCP/%s:%d] %s\n", addr, ntohs(clientAddr.sin_port), buf);
-            
-            // send data
-            retval = send(client_sock, buf, retval, 0);
-            if (retval == -1) {
-                err_display("send()");
-                break;
-            }
-        }
-        close(client_sock);
-        printf("[TCP Server] IP: %s:%d\n", addr, ntohl(clientAddr.sin_port));
+        // create thread
+        retval = pthread_create(&tid, NULL, ProcessClient, (void *)(long long)client_sock);
+        if (retval != 0) { close(client_sock); }
     }
-    
-
     
     close(client_sock);
     return 0;
